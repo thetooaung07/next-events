724df208bc25d59f08f3027059cf7eaacc58c30c
thetooaung07 <thetooaung07@gmail.com>
2021-07-12 11:24:08 +0630

    Add Dummy Images and components folder

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 import React from "react";
+import { getFeaturedEvents } from "../dummy-data";
 
 function HomePage() {
+
+  const featureEvents = getFeaturedEvents();
+
   return (
     <div>
       <h1>The Home Page</h1>
