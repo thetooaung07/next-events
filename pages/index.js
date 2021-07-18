19ddd0beac31d8643d9de08e5bf99a74d768702c
thetooaung07 <thetooaung07@gmail.com>
2021-07-18 10:52:03 +0630

    Remove all testing codes

--- pages/index.js
+++ pages/index.js
@@ -10,42 +10,12 @@ function HomePage({ products }) {
 
   return (
     <div>
-      {products.map((product) => (
-        <li key={product.id}>{product.title}</li>
-      ))}
-      {/* <EventList items={featureEvents}></EventList> */}
+    
+      <EventList items={featureEvents}></EventList>
     </div>
   );
 }
 
-export async function getStaticProps(context) {
 
-  console.log(`context in getStaticProps => ${context}`);
-  const filePath = path.join(process.cwd(), "data", "dummy-data.json");
-  const jsonData = await fs.readFileSync(filePath);
-  const data = JSON.parse(jsonData);
-  console.log(`filePath => ${filePath}`);
-  console.log(`jsonData => ${jsonData}`);
-  console.log(`data => ${data}`);
-
-  if (!data) {
-    return {
-      redirect: {
-        destination: "/no-data",
-      },
-    };
-  }
-
-  if (data.products.length === 0) {
-    return { notFound: true };
-  }
-
-  return {
-    props: {
-      products: data.products,
-    },
-    revalidate: 10,
-  };
-}
 
 export default HomePage;
