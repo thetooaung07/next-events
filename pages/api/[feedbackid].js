c972983ed75fc9e117eaf86569004bdc317e4cb4
thetooaung07 <thetooaung07@gmail.com>
2021-07-21 17:55:49 +0630

    #APi dynamic route api testing

---
+++ pages/api/[feedbackid].js
@@ -0,0 +1,17 @@
+import { buildFeedbackPath, extractFeedback } from "./feedback";
+
+function handler(req, res) {
+  const feedbackId = req.query.feedbackid;
+  const filePath = buildFeedbackPath();
+  const feedbackData = extractFeedback(filePath);
+
+  const selectedFeedback = feedbackData.find(
+    (feedback) => feedback.id === feedbackId
+  );
+
+  res.status(200).json({
+    feedback: selectedFeedback,
+  });
+}
+
+export default handler;
---
+++ pages/feedback/index.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { buildFeedbackPath, extractFeedback } from "../api/feedback";
+
+function FeedBackPage(props) {
+  const [feedbackData, setFeedbackData] = useState();
+
+  function loadFeedbackHandler(id) {
+    fetch(`/api/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setFeedbackData(data.feedback);
+      });
+  }
+
+  return (
+    <>
+      {feedbackData && <p>{feedbackData.email}</p>}
+      <ul>
+        {props.feedbackItems.map((item) => (
+          <li key={item.id}>
+            {item.feedback}
+            <button
+              //  onClick={() => loadFeedbackHandler(id)} use this or
+              onClick={loadFeedbackHandler.bind(null, item.id)}
+              //  .bind() method is to pre-configure things like parameters and not immediately execute function
+            >
+              Show Details
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  // you should not use fetch api or http request to your own server in here (getStaticProps)
+  // just import them directly and don't added up unnecessary http request
+
+  const filePath = buildFeedbackPath();
+  const data = extractFeedback(filePath);
+
+  return {
+    props: {
+      feedbackItems: data,
+    },
+  };
+}
+
+export default FeedBackPage;
