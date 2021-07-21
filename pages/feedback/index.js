4fad3a6ae82e0849f9e85da919260aaaa2f643d2
thetooaung07 <thetooaung07@gmail.com>
2021-07-21 18:04:31 +0630

    #APi restructuring api paths

--- pages/feedback/index.js
+++ pages/feedback/index.js
@@ -1,11 +1,11 @@
 import { useState } from "react";
-import { buildFeedbackPath, extractFeedback } from "../api/feedback";
+import { buildFeedbackPath, extractFeedback } from "../api/feedback/index";
 
 function FeedBackPage(props) {
   const [feedbackData, setFeedbackData] = useState();
 
   function loadFeedbackHandler(id) {
-    fetch(`/api/${id}`)
+    fetch(`/api/feedback/${id}`)
       .then((res) => res.json())
       .then((data) => {
         setFeedbackData(data.feedback);
